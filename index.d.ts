708161f26f7d50375209b3f14b2eb8b383b1670a
zubquzaini <zubliquzaini@gmail.com>
2021-01-25 17:06:05 +0800

    first commit

---
+++ index.d.ts
@@ -0,0 +1,81 @@
+declare namespace BillplzV4 {
+    // api options type
+    interface BillplzOptions {
+        key: string;
+        endpoint?: string;
+        sandbox?: boolean;
+    }
+
+    // collection type
+    interface CollectionArguments {
+        title: string;
+    }
+
+    // open collection type
+    interface OpenCollectionArguments {
+        title: string;
+        description: string;
+        amount: number;
+        fixed_amount?: boolean;
+        fixed_quantity?: boolean;
+        payment_button?: string;
+        reference_1_label?: string
+        reference_2_label?: string;
+        email_link?: string;
+        tax?: number;
+    }
+
+    // bill type
+    interface BillArguments {
+        collection_id: string;
+        email: string;
+        mobile: number;
+        name: string;
+        amount: number;
+        callback_url: string;
+        description: string;
+        due_at?: string;
+        redirect_url?: string;
+        deliver?: boolean;
+        reference_1_label?: string;
+        reference_1?: string;
+        reference_2_label?: string;
+        reference_2?: string;
+    }
+
+    // payout argument
+    interface PayoutArgument {
+        
+    }
+}
+
+declare class BillplzV4 {
+    // constructor
+    constructor(options: string | BillplzV4.BillplzOptions);
+
+    // create_collection
+    create_collection(title: BillplzV4.CollectionArguments, callback?: (res: any, err: any) => void): void;
+
+    // create collectionOpen
+    create_collectionOpen(args: BillplzV4.OpenCollectionArguments, callback?: (res: any, err: any) => void): void;
+
+    // create bill
+    create_bill(args: BillplzV4.BillArguments, callback?: (res: any, err: any) => void): void;
+
+    // get bill
+    get_bill(billId: string, callback?: (res: any, err: any) => void): void;
+
+    // delete bill
+    delete_bill(billId: string, callback?: (res: any, err: any) => void): void;
+
+    // change_collection status
+    change_collection_status(collectionId: string, status: string, callback?: (res: any, err: any) => void): void;
+
+    // registration check
+    registration_check(bankAccountNumber: string, callback?: (res: any, err: any) => void): void;
+
+    /// create payout
+    create_payout(args: BillplzV4.PayoutArgument, callback?: (res: any, err: any) => void): void;
+}
+
+export = BillplzV4;
---
+++ index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const Wreck = require('wreck')
+let wreck
+
+module.exports = class BillplzV4 {
+
+  constructor(options) {
+
+    this._apiKey = null
+
+    this._apiEndpoint = 'https://www.billplz.com/api/v3/'
+    this._sandboxApiEndpoint = 'https://billplz-staging.herokuapp.com/api/v3/'
+
+    this._apiEndpoint_v4 = 'https://www.billplz.com/api/v4/'
+    this._sandboxApiEndpoint_v4 = 'https://billplz-staging.herokuapp.com/api/v4/'
+
+    this._isSandbox = false
+
+    if (typeof options === 'object') {
+      this._apiKey = options.key || this._apiKey
+      this._apiEndpoint = options.endpoint || this._apiEndpoint
+      this._sandboxApiEndpoint = options.sandboxEndpoint || this._sandboxApiEndpoint
+      this._isSandbox = options.sandbox || this._isSandbox
+    }
+    else {
+      this._apiKey = options
+    }
+
+    if (this._isSandbox) {
+      this._apiEndpoint = this._sandboxApiEndpoint
+    }
+
+    wreck =  Wreck.defaults({
+      headers: { 'Authorization': 'Basic ' + new Buffer(this._apiKey).toString('base64') }
+    });
+  }
+
+  request(method, params, callback) {
+    wreck.post(this._apiEndpoint + method, {
+        payload: params
+      }, (err, res, payload) => {
+        callback(err, JSON.parse(payload.toString()))
+    });
+  }
+
+  requestv4(method, params, callback) {
+    wreck.post(this._apiEndpoint_v4 + method, {
+        payload: params
+      }, (err, res, payload) => {
+        callback(err, JSON.parse(payload.toString()))
+    });
+  }
+
+  //create collection
+  create_collection(params, callback) {
+    return this.request('collections', params, callback)
+  }
+
+  //create open collection
+  create_collectionOpen(params, callback) {
+    return this.request('open_collections', params, callback)
+  }
+
+  //create bill
+  create_bill(params, callback) {
+    return this.request('bills', params, callback)
+  }
+
+  //get bill
+  get_bill(billId, callback) {
+    wreck.get(this._apiEndpoint + 'bills/' + billId, (err, res, payload) => {
+      callback(err, JSON.parse(payload.toString()))
+    });
+  }
+
+  //delete bill
+  delete_bill(billId, callback) {
+    wreck.delete(this._apiEndpoint + 'bills/' + billId, (err, res, payload) => {
+      callback(err, JSON.parse(payload.toString()))
+    });
+  }
+
+  //change collection status
+  change_collection_status(collectionId, status, callback) {
+    wreck.post(this._apiEndpoint + 'collections/' + collectionId + '/' + status, (err, res, payload) => {
+      callback(err, JSON.parse(payload.toString()))
+    });
+  }
+
+  //registration check
+  registration_check(bankAccountNumber, callback) {
+    wreck.get(this._apiEndpoint + 'check/bank_account_number/' + bankAccountNumber, (err, res, payload) => {
+      callback(err, JSON.parse(payload.toString()))
+    });
+  }
+
+  ///payout method
+  create_payout(params, callback) {
+    return this.requestv4('mass_payment_instructions', params, callback);
+  }
+}
